4177082854532650982aaaecabe54f9fb28b2294
leandro-gft <lpsacchi@gmail.com>
2020-09-11 13:17:47 -0300

    Utilizando o MessageListener

--- src/br/com/caelum/jms/TesteConsumidor.java
+++ src/br/com/caelum/jms/TesteConsumidor.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
@@ -27,9 +30,22 @@ public class TesteConsumidor {
 		
 		Destination fila = (Destination) context.lookup("financeiro");//local concreto onde a mensagem será salva temporariamente ,dentro do MOM
 		MessageConsumer consumer = session.createConsumer(fila); //consumer fica escutando as mensagens de uma fila concreta
+					
+//		Message message = consumer.receive(); //recebedor da mensagem devolve uma mensagem;
 		
-		Message message = consumer.receive(); //recebedor da mensagem devolve uma mensagem;
-		System.out.println("Recebendo mensagem: "+message);
+		consumer.setMessageListener(new MessageListener() { 
+			//MessageListener permite que o consumer fique escutando e não termine quando recebe uma mensagem
+			
+			@Override
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+				try {
+					System.out.println(textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
 		new Scanner(System.in).nextLine();
 		
